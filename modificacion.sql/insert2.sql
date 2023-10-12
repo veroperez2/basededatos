babdd808881fd6c4b461e97420c6ad572396c82f
Rodrigo Villca <veroperezz391@gmail.com>
2023-10-12 09:26:03 -0300

    ejercicio 2 navarro

---
+++ modificacion.sql/insert2.sql
@@ -0,0 +1,2 @@
+DELETE "user" 
+                             "pasword"
---
+++ views.sql/ejercicio1.sql
@@ -0,0 +1,6 @@
+CREATE VIEW peliculas_rating as
+SELECT conunt (title)as peliculas,rating
+      FROM BY rating
+
+CREATE VIEW peliculas_categoria as
+SELECT 
